1b498462addc3ba7163f97d899e63c83c3a53b2d
Stephen Kopylov <stephen@expertoption.com>
2016-06-07 17:29:07 +0300

    Refactoring

--- Classes/SKAsyncGLView.h
+++ Classes/SKAsyncGLView.h
@@ -28,7 +28,7 @@
 @property (nonatomic) GLuint framebuffer;
 @property (nonatomic, strong) EAGLContext *mainContext;
 @property (nonatomic, strong) EAGLContext *renderContext;
-@property (nonatomic, getter = isRenderable) BOOL renderable;
+@property (nonatomic) BOOL log;
 
 @property (nonatomic, weak) id<SKAsyncGLViewDelegate> delegate;
 
